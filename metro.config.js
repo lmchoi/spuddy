4f6aa0f6df2366609595ac9ad1bbad00fa287303
mandy <hello@mandychoi.com.au>
2026-05-25 19:28:23 +0100

    fix: block expo-sqlite worker in metro config to prevent web bundling errors

--- metro.config.js
+++ metro.config.js
@@ -7,9 +7,11 @@ const config = getDefaultConfig(__dirname);
 
 // expo-sqlite uses a WASM web worker that Metro can't resolve on web.
 // Block the import so web bundling doesn't fail.
+// We use blockList for native and also ensure it's handled for web.
 config.resolver.blockList = [
   ...(config.resolver.blockList ?? []),
   /expo-sqlite\/web\/wa-sqlite\/.*\.wasm/,
+  /expo-sqlite\/web\/worker\.ts/,
 ];
 
 module.exports = withStorybook(withNativewind(config), {
